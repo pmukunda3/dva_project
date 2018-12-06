c0e3d24506ca5faefd3482397051e636b5332297
Rajdeep Singh <rajdeep114@gmail.com>
2018-12-05 22:28:33 -0600

    remove node modules

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="full-width center-content">
-    <h1>This is a view csdc hkjh khj hjkjh . </h1>
-  </div>
-</template>
-<style scoped>
-@import './styles/app.css';
-</style>
